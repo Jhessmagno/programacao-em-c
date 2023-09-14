ad889387576aee2813d211b6441105aa2e4e4ffd
Jhessmagno <jessicakaroline1102@gmail.com>
2023-09-14 19:49:06 -0400

    add files C

---
+++ Hello-world.c
@@ -0,0 +1,15 @@
+//biblioteca que faz invocar funcionalidades e comandos que sao utilizados para que o usuario entenda o que esta fazer.
+#include <stdio.h>
+
+//funcao de entrada e saida pre programada que sao da biblioteca c
+
+int main() {
+	//tudo que estiver entre as chaves vai ser executado pela maquina e só o que tiver entre as chaves vai ser executado pela maquina.
+	printf("Hello, world!\n"); // funcao de entrada e saida e dentro dos parenteses coloca o texto.
+}
+
+//anotacoes
+// para executar o programa primeirotenho que copilar.
+// o que é compilar:  o meu ambiente de programacaao junto com o meu compilador vai analiasar e vai transformar esse codigo executavel.
+// que é o exe., no caso F9. l.á ira mostrar os erros etc.  e em Run(F10) vai rodar o codigo
+// o que é o \n (contra barra n)? é uma especie de sub comando  para gerar uma quebra de linha(pula de linha)
